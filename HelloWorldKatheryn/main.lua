8ceaf9d156221046e67d54e83f5637dc08a19d3a
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-10-01 13:53:07 -0400

    added moving images

--- HelloWorldKatheryn/main.lua
+++ HelloWorldKatheryn/main.lua
@@ -5,4 +5,27 @@
 --terminal.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--print "Hello, World" to the command terminal
+print ("***Hellooooooo")
+
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--sets the background colour
+display.setDefault("background", 115/255, 230/255, 0/255)
+
+--create a local variable
+local textObject
+local textObjectSignature
+
+--displays text on the screen at the position x = 400 and y = 400 with
+--a deafult font style and font size of 45
+textObject = display.newText("Heyyyy, Katheryn!", 455, 455, nil, 70 )
+
+--displays text on the screen at the position x = 500 and y = 500 with
+--a deafult font style and font size of 45
+textObjectSignature = display.newText("Created by Katheryn B.M", 600,600, nil, 65 )
+
+--sets the color of the text
+textObject:setTextColor(150/255, 0/255, 127/255 )
+textObjectSignature:setTextColor(0/255, 0/255, 150/255 )
