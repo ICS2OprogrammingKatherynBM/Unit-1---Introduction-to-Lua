21fc44ec28aae94d1d289d335d8ab4c964b994fa
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-10-03 14:24:55 -0400

    did small changes

--- TouchDragKatheryn/main.lua
+++ TouchDragKatheryn/main.lua
@@ -1,7 +1,13 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title:TouchandDrag
+--Name: Katheryn 
+--course: ICS2O
+-- This program displays images that react to a person's finger.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--local Variables.I am still trying to get the x-value to be set properly
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local blueGirl = display.newImageRect("Images/blueGirl.png", 150, 150)
