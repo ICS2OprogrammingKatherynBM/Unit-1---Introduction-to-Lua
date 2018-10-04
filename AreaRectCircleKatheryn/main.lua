521dd6601e616a84344742c0869de5d65b90cb38
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-10-04 16:41:41 -0400

    small changes

--- AreaRectCircleKatheryn/main.lua
+++ AreaRectCircleKatheryn/main.lua
@@ -6,15 +6,22 @@
 -----------------------------------------------------------------------------------------
 
 -- Create my local variables
-local areatext 
+local areatext
+local areaTextCircle 
 local textsize = 50
 local myRectangle 
 local widthOfRectangle = 350 
 local heightOfRectangle = 200
 local areaOfRectangle
+local areaOfCircle
+local radiusOfCircle = 95
+local PI = 3.14
+local myCircle = display.newCircle(500, 500, 75) 
+
 
 -- set the backround colour of my screen.Remember that colors are between 0 and 1.
 display.setDefault("background" , 145/255, 0/255, 200/255)
+
 --to remove status bar 
 display.setStatusBar(display.HiddenStatusBar)
 
@@ -31,4 +38,39 @@ myRectangle.y = 40
 myRectangle.strokeWidth = 20
 
 --set the colour of the rectangle
-myRectangle:setFillColour(0.7, 0.1, 0.3)
+myRectangle:setFillColor(0.7, 0.1, 0.3)
+
+--set the color of the border
+myRectangle:setStrokeColor(1, 0, 1)
+
+--calculate the area
+areaOfRectangle = widthOfRectangle * heightOfRectangle
+
+--calculate the area of the circle 
+areaOfCircle = PI * radiusOfCircle * radiusOfCircle
+
+--write the area on the screen. Take into consideration the size of the font when positioning it on the screen
+areaText = display.newText("The area of this rectangle with a width of \n"..
+				widthOfRectangle.." and a height of ".. heightOfRectangle.." is "..
+				areaOfRectangle.. "pixels².", 0, 0, Arial, 45)
+
+--write area for circle
+areaTextCircle = display.newText("The area of this circle with a radius of\n"..
+					radiusOfCircle .." is ".. areaOfCircle.." pixels².",0, 0, Arial, 45)
+
+--anchor the text and set the its (x,y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 20
+areaText.y = display.contentHeight/3
+
+--set the color of the newText
+areaText:setTextColor( 0/255, 200/255, 222/255)
+
+--anchor the text and set the its (x,y) position
+areaOfCircle.anchorX = 0
+areaOfCircle.anchorY = 0
+areaTextCircle.x =20
+areaTextCircle.y =display.contentHeight
+
+
--- MovingImagesKatheryn/main.lua
+++ MovingImagesKatheryn/main.lua
@@ -13,15 +13,25 @@ scrollSpeed = 3
 --background image with width and height
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
 
---character image with width anf height
+--character image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- second character image with width and height
+local heart = display.newImageRect("Images/healthheart.png", 100, 100)
 
 --set the image to be transparent
 beetleship.alpha = 0
 
+--second image to be transparent
+heart.alpha = 1
+
 -- set the initial x and y position of beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
+
+--set the initial x and y position of heart
+heart.x = 1048
+heart.y = display.contentHeight/2
 
 --function: MoveShip
 --Input: this function accepts an event listener
@@ -36,3 +46,18 @@ end
 
 --Moveship will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+ 
+
+--function: MoveHeart
+--Input: this function accepts an event listener
+--Output: none
+--Discription: this function adds the scroll speed to the x-value of the heart
+local function MoveHeart(event)
+	-- add the scroll speed to the x-value of the heart
+	heart.x = heart.x - scrollSpeed
+	--change the transparency of the ship every time it moves so that it fades out
+	heart.alpha = heart.alpha - 0.001
+end
+
+--MoveHeart will be called over and over again
+Runtime:addEventListener("enterFrame", MoveHeart)
