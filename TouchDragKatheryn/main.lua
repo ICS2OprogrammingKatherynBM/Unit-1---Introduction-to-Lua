9429371ed05844f0a730ec2293e566c68cdfe7e0
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-10-15 15:18:14 -0400

    Update TouchDragKatheryn/main.lua

--- TouchDragKatheryn/main.lua
+++ TouchDragKatheryn/main.lua
@@ -12,4 +12,67 @@ display.setStatusBar(display.HiddenStatusBar)
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
 local blueGirl = display.newImageRect("Images/blueGirl.png", 150, 150)
 local blueGirlWidth = blueGirl.width
+local blueGirlHeight = blueGirl.height
 
+local bluePrincess =  display.newImageRect("Images/bluePrincess.png", 300, 300)
+local bluePrincessWidth = bluePrincess.width
+local bluePrincessHeight = bluePrincess.height
+
+-- My boolean variables to keep track of which object I touched first
+local alreadyTouchedBlueGirl = false
+local alreadyTouchedBluePrincess = false
+
+--set the intial x and y postion of myImages 
+blueGirl.x = 400
+blueGirl.y = 500
+
+bluePrincess.x = 300
+bluePrincess.y = 200
+
+-- Function: BlueGirlListener
+-- Input: touch listener
+-- Output: none
+-- Discription: when blue girl is touched, move her
+local function BlueGirlListener(touch)
+	if(touch.phase == "began") then
+		if (alreadyTouchedBlueGirl== false) then 
+			alreadyTouchedBluePrincess = true
+		end
+	end
+
+	 if ( (touch.phase == "moved" ) and (alreadyTouchedBluePrincess == true) ) then
+	 	bluePrincess.x = touch.x
+	 	bluePrincess.y = touch.y
+	end
+
+	if (touch.phase == "ended") then
+		alreadyTouchedBlueGirl = false
+		alreadyTouchedBluePrincess = false
+	end
+end
+
+-- add the resoective listeners to each object
+bluePrincess:addEventListener("touch", BlueGirlListener)
+
+-- Another object move
+
+local function BluePrincessListener(touch)
+	if(touch.phase == "began") then
+		if (alreadyTouchedbluePrincess== false) then 
+			alreadyTouchedBlueGirl = true
+		end
+	end
+
+	 if ( (touch.phase == "moved" ) and (alreadyTouchedBlueGirl == true) ) then
+	 	blueGirl.x = touch.x
+	 	blueGirl.y = touch.y
+	end
+
+	if (touch.phase == "ended") then
+		alreadyTouchedBlueGirl = false
+		alreadyTouchedBluePrincess = false
+	end
+end
+
+-- add the resoective listeners to each object
+bluePrincess:addEventListener("touch", BlueGirlListener)
