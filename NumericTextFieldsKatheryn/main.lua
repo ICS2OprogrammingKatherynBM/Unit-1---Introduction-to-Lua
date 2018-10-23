f80d112f202dd8dea536ef4e468685355c2c5b50
katherynbohl-martinez <43419119+katherynbohl-martinez@users.noreply.github.com>
2018-10-23 15:00:28 -0400

    gg

--- NumericTextFieldsKatheryn/main.lua
+++ NumericTextFieldsKatheryn/main.lua
@@ -30,7 +30,7 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
-local incorrectAnswer
+local incorrectAnswer2
 local randonOperator
 local correctSound
 local incorrectSound
@@ -38,6 +38,22 @@ local incorrectSoundChannel
 local correctAnswers
 local numberOfPoints = 0
 local pointsObjects
+local points
+-----------------------------------------------------------------------------------------
+--LCOAL VARIABLES FOR TIMER AND LIVES
+-----------------------------------------------------------------------------------------
+local totalSeconds = 5 
+local secondsLeft = 5 
+local clockText 
+local countDownTimer
+
+local lives = 4
+local heart1
+local heart2
+local heart3
+local heart4
+
+--*** ADD LOCAL VARIABLES FOR : INCORRECT OBJECT, POINTS OBJECT, POINTS
 -----------------------------------------------------------------------------------------
 --SOUNDS
 -----------------------------------------------------------------------------------------
@@ -89,15 +105,63 @@ local function AskQuestion()
 	end
 end
 
+
+local function UpdateTime()
+
+	--decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	--display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0 ) then
+		--reset the number of seconds left
+		secondsLeft = totalSeconds
+		lives = lives - 1
+		
+
+
+		--***CALL THE FUNCTION TO ASK A NEW QUESTIION
+	end
+end
+
+
+local function UpdateLives()
+
+	if (lives == 3) then 
+		heart4.isVisible = false
+	elseif (lives == 2) then 
+		heart3.isVisible = false
+	elseif (lives == 1) then
+		heart2.isVisible = false
+	elseif (lives == 0) then 
+		heart1.isVisible = false
+	end
+end
+
+
+--function that calls the timer
+local function StartTimer()
+	--create a countdown timer that loops infinitely 
+	countDownTimer = timer.performWithDelay(1000, UpdateTime, 0)
+end
+
+
+
+
 local function HideCorrect()
 	correctObject.isVisible = false
 	AskQuestion()
 end
 
+
+
 local function HideIncorrect()
 	incorrectObject.isVisible = false
 	AskQuestion()
 end 
+
+
 
 
 local function NumericFieldListener(event)
@@ -126,6 +190,10 @@ local function NumericFieldListener(event)
 		else
 			correctObject.isVisible = false
 			incorrectObject.isVisible = true
+			lives = lives - 1
+
+			 --call the function
+			 UpdateLives()
 
 
 			incorrectSoundChannel = audio.play(incorrectSound)
@@ -139,7 +207,6 @@ local function NumericFieldListener(event)
 	end
 end
 
-
 -----------------------------------------------------------------------------------------
 --OBJECT CREATION
 -----------------------------------------------------------------------------------------
@@ -148,13 +215,18 @@ end
 questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50 )
 questionObject:setTextColor( 102/255, 255/255, 255/255)
 
+
 --display number of points
-pointsObjects = display.newText("points = " .. numberOfPoints , display.contentWidth/2, display.contentHeight/4, nil, 50)
+pointsObjects = display.newText("", display.contentWidth/2, display.contentHeight/4, nil, 50)
+pointsObjects.text = ("points = " .. numberOfPoints)
+
 
 --create the correct text object and make it invisible 
 correctObject = display.newText(" Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
 correctObject:setTextColor( 255/255, 255/255, 102/255)
 correctObject.isVisible = false
+
+clockText = display.newText("", display.contentWidth * 4 / 5, display.contentHeight * 3/ 5, nil, 50)
 
 --create the incorrect text object and make it invisible
 incorrectObject = display.newText(" Incorrect ", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
@@ -163,6 +235,28 @@ incorrectObject.isVisible = false
 -- Create numeric field 
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
 numericField.inputType = "number"
+
+
+--create the lives to display on screen
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8 
+heart1.y = display.contentHeight * 1 / 7
+
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8 
+heart2.y = display.contentHeight * 1 / 7
+
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8 
+heart3.y = display.contentHeight * 1 / 7
+
+
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8 
+heart4.y = display.contentHeight * 1 / 7
+
 
 --add the event listener for the numeric field
 numericField:addEventListener("userInput", NumericFieldListener)
@@ -175,4 +269,8 @@ numericField:addEventListener("userInput", NumericFieldListener)
 --call the function to ask the questions
 AskQuestion()
 
+--call the functions
+UpdateTime()
 
+--call the functions
+StartTimer()
